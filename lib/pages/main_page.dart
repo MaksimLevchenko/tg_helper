25ca2a4e73dba5f8970125731ab9b4e38f868b03
EliteNoobest <Valvekat@mail.ru>
2024-04-28 18:50:43 +0300

    Refactor MainPage to use a StatefulWidget and implement bottom navigation bar functionality

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -1,11 +1,40 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_tg_helper/style/text_style.dart';
 
-class MainPage extends StatelessWidget {
+class MainPage extends StatefulWidget {
   const MainPage({super.key});
 
   @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  int bottomAppBarValue = 0;
+
+  Widget homePage() {
+    return const Center(
+      child: Text('Welcome to the main page!'),
+    );
+  }
+
+  Widget settingsPage() {
+    return const Center(
+      child: Text('Settings page'),
+    );
+  }
+
+  void onSelectTab(int index) {
+    setState(() {
+      bottomAppBarValue = index;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final List<Widget> widgetList = [
+      homePage(),
+      settingsPage(),
+    ];
     return Scaffold(
         appBar: AppBar(
           title: Text(
@@ -16,10 +45,10 @@ class MainPage extends StatelessWidget {
             ),
           ),
         ),
-        body: const Center(
-          child: Text('Welcome to the main page!'),
-        ),
+        body: widgetList[bottomAppBarValue],
         bottomNavigationBar: BottomNavigationBar(
+          onTap: onSelectTab,
+          currentIndex: bottomAppBarValue,
           items: const [
             BottomNavigationBarItem(
               icon: Icon(Icons.home),
