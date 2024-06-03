5f5f4bf33255323428f8536376c3042bd42554b5
Maksim Levchenko <36772088+MaksimLevchenko@users.noreply.github.com>
2024-06-03 21:00:03 +0300

    Update app_api.dart

--- lib/app_api.dart
+++ lib/app_api.dart
@@ -1,4 +1,4 @@
 class TgApi {
-  static const int apiId = 24056288;
-  static const String apiHash = 'f3f99d6ecee6d0ebd7684924446a8dbf';
+  static const int apiId = ;
+  static const String apiHash = '';
 }
